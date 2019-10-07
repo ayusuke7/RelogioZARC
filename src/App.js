6b39904c683ee781ed291f0efab5898a9d01926b
ayusuke7 <alexhcb.7392@gmail.com>
2019-10-06 22:06:20 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  gridGenerator = cols => {
+    return (
+      <div className="grid">
+        {[...Array(18)].map((it, id) => (
+          <div key={id} className="line">
+            {[...Array(18)].map((it, id) => (
+              <div key={id} className="cell"></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  spinnerGenerator = () => {
+    return (
+      <div className="spinner">
+        <div className="ball"></div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="clock">
+          {/* this.gridGenerator() */}
+          {/* this.spinnerGenerator() */}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
