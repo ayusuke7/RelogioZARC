ac829ee1fd1066ed27601189926a10539920b4be
ahbarbosa <ahbarbosa@brasilseg.com.br>
2019-10-08 10:35:10 -0300

    c

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
 import ClockZarc from "./ClockZarc";
+import moment from "moment";
+
 import "./App.scss";
 
+const now = moment().format("YYYY-MM-DD")
+
 class App extends Component {
+  state = {
+    hoje: now,
+    zarc: [
+      "",0,0,0, 
+      "",0,0,0,
+      "",0,0,0,
+      "",0,0,0,
+      "",0,0,0,
+      "",0,0,0,
+      "",0,0,0,
+      "",0,0,0,
+      "",0,0,20,
+      "",20,20,20,
+      "",20,20,20,
+      "",20,20,20
+    ]
+  };
+
+
+  calculate = (month, day) => {
+    
+  }
+
   render() {
+    const { hoje, zarc } = this.state;
     return (
       <div className="container">
-        <ClockZarc />
+        <ClockZarc
+          zarc={zarc}
+          title={`HOJE: ${hoje}`}
+          subtitle={"faltam 0 dia paras o início do ZARC *"}
+        />
       </div>
     );
   }
--- src/ClockZarc.js
+++ src/ClockZarc.js
@@ -2,11 +2,76 @@ import React, { Component } from "react";
 import "./ClockZarc.scss";
 
 class ClockZarc extends Component {
+  pureCircle = () => (
+    <>
+      <div>{/* Janeiro */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Fevereiro */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Março */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Abril */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Maio */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Junho */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Julho */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Agosto */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Setembro */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div>{/* Outubro */}</div>
+      <div className="ball blue"></div>
+      <div className="ball blue"></div>
+      <div className="ball blue"></div>
+      <div>{/* Novembro */}</div>
+      <div className="ball green"></div>
+      <div className="ball yellow"></div>
+      <div className="ball"></div>
+      <div>{/* Dezembro */}</div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+      <div className="ball"></div>
+    </>
+  );
+
+  colorChange = percentage => {
+    if(percentage !== ""){
+      
+    }
+  };
+
   render() {
+    const { title, subtitle, zarc = [] } = this.props;
+    const color = "blue";
+
     return (
       <div>
         <div className="border">
-          <div className="message">HOJE: 28 de Agosto</div>
+          <div className="labels">
+            <div className="title">{title || ""}</div>
+            <div className="subtitle">{subtitle || ""}</div>
+          </div>
         </div>
         <div className="circle-months">
           <div>JAN</div>
@@ -23,54 +88,13 @@ class ClockZarc extends Component {
           <div>DEZ</div>
         </div>
         <div className="circle-days">
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball blue"></div>
-          <div className="ball blue"></div>
-          <div className="ball blue"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
+          {this.pureCircle()}
+          {/* zarc.map((item, index) => {
+            if (index % 4 === 0) 
+              return <div></div>;
+            else 
+            return <div className={"ball " + color}></div>;
+          }) */}
         </div>
       </div>
     );
