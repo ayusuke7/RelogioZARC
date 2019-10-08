0749ed69b86642976eb35cf78695126c263c5350
ayusuke7 <alexhcb.7392@gmail.com>
2019-10-08 00:50:36 -0300

    c

--- src/App.js
+++ src/App.js
@@ -1,75 +1,12 @@
 import React, { Component } from "react";
+import ClockZarc from "./ClockZarc";
 import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="border"></div>
-        <div className="circle-months">
-          <div>JAN</div>
-          <div>FEV</div>
-          <div>MAR</div>
-          <div>ABR</div>
-          <div>MAI</div>
-          <div>JUN</div>
-          <div>JUL</div>
-          <div>AGO</div>
-          <div>SET</div>
-          <div>OUT</div>
-          <div>NOV</div>
-          <div>DEZ</div>
-        </div>
-        <div className="circle-days">
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball blue"></div>
-          <div className="ball blue"></div>
-          <div className="ball blue"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-          <div className="ball"></div>
-        </div>
+        <ClockZarc />
       </div>
     );
   }
---
+++ src/ClockZarc.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import "./ClockZarc.scss";
+
+class ClockZarc extends Component {
+  render() {
+    return (
+      <div>
+        <div className="border">
+          <div className="message">HOJE: 28 de Agosto</div>
+        </div>
+        <div className="circle-months">
+          <div>JAN</div>
+          <div>FEV</div>
+          <div>MAR</div>
+          <div>ABR</div>
+          <div>MAI</div>
+          <div>JUN</div>
+          <div>JUL</div>
+          <div>AGO</div>
+          <div>SET</div>
+          <div>OUT</div>
+          <div>NOV</div>
+          <div>DEZ</div>
+        </div>
+        <div className="circle-days">
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball blue"></div>
+          <div className="ball blue"></div>
+          <div className="ball blue"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+          <div className="ball"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ClockZarc;
